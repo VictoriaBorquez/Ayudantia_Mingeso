5cf8bfb3673927055c1718b6628146a06d650bb2
VictoriaBorquez <victoria.borquez@usach.cl>
2022-10-07 23:39:22 -0300

    Microservicios

---
+++ Microservicios/user-service/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}

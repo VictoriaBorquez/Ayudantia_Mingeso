051b9758adeaead271acbdf3043fc06269b09db5
VictoriaBorquez <victoria.borquez@usach.cl>
2022-10-27 15:33:01 -0300

    Microservicios + React + Docker

---
+++ Microservicios_React_Docker/user-service/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.model.Car;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping()
+	public ResponseEntity<List<User>> getAll(){
+		List<User> users = userService.getAll();
+		if(users.isEmpty())
+			return ResponseEntity.noContent().build();
+		return ResponseEntity.ok(users);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getById(@PathVariable("id") int id) {
+		User user = userService.getUserById(id);
+		if(user == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(user);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<User> save(@RequestBody User user) {
+		User userNew = userService.save(user);
+		return ResponseEntity.ok(userNew);
+	}
+	
+	@GetMapping("/cars/{userId}")
+	public ResponseEntity<List<Car>> getCars(@PathVariable("userId") int userId){
+		User user = userService.getUserById(userId);
+		if(user == null)
+			return ResponseEntity.notFound().build();
+		List<Car> cars = userService.getCars(userId);
+		return ResponseEntity.ok(cars);
+	}
+	
+	
+	
+	
+
+}
